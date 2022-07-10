c93c7f6f79db1f4dba7b8cc951c76205f6bd7921
ManhNguyen98 <nguyenthietmanh98@gmail.com>
2022-07-10 16:24:26 +0700

    rebuild

--- src/components/DocketNote.jsx
+++ src/components/DocketNote.jsx
@@ -4,56 +4,56 @@ import { ListNote } from './ListNote';
 import Panel from './Panel';
 import SearchField from './SearchField';
 
-const NOTES = [
-  {
-    id: 0,
-    content:
-      'The beginning of screenless design: UI jobs to be taken over by Solution Architect',
-    createdAt: 1657432075944,
-    isStarred: false,
-    color: '#FFA940',
-  },
-  {
-    id: 1,
-    content:
-      'The beginning of screenless design: UI jobs to be taken over by Solution Architect',
-    createdAt: 1657432075944,
-    isStarred: true,
-    color: '#FADB14',
-  },
-  {
-    id: 2,
-    content:
-      'The beginning of screenless design: UI jobs to be taken over by Solution Architect',
-    createdAt: 1657432075944,
-    isStarred: false,
-    color: '#FFA940',
-  },
-  {
-    id: 3,
-    content:
-      'The beginning of screenless design: UI jobs to be taken over by Solution Architect',
-    createdAt: 1657432075944,
-    isStarred: false,
-    color: '#2F54EB',
-  },
-  {
-    id: 4,
-    content:
-      'The beginning of screenless design: UI jobs to be taken over by Solution Architect',
-    createdAt: 1657432075944,
-    isStarred: true,
-    color: '#20C950',
-  },
-  {
-    id: 5,
-    content:
-      'The beginning of screenless design: UI jobs to be taken over by Solution Architect',
-    createdAt: 1657432075944,
-    isStarred: false,
-    color: '#722ED1',
-  },
-];
+// const NOTES = [
+//   {
+//     id: 0,
+//     content:
+//       'The beginning of screenless design: UI jobs to be taken over by Solution Architect',
+//     createdAt: 1657432075944,
+//     isStarred: false,
+//     color: '#FFA940',
+//   },
+//   {
+//     id: 1,
+//     content:
+//       'The beginning of screenless design: UI jobs to be taken over by Solution Architect',
+//     createdAt: 1657432075944,
+//     isStarred: true,
+//     color: '#FADB14',
+//   },
+//   {
+//     id: 2,
+//     content:
+//       'The beginning of screenless design: UI jobs to be taken over by Solution Architect',
+//     createdAt: 1657432075944,
+//     isStarred: false,
+//     color: '#FFA940',
+//   },
+//   {
+//     id: 3,
+//     content:
+//       'The beginning of screenless design: UI jobs to be taken over by Solution Architect',
+//     createdAt: 1657432075944,
+//     isStarred: false,
+//     color: '#2F54EB',
+//   },
+//   {
+//     id: 4,
+//     content:
+//       'The beginning of screenless design: UI jobs to be taken over by Solution Architect',
+//     createdAt: 1657432075944,
+//     isStarred: true,
+//     color: '#20C950',
+//   },
+//   {
+//     id: 5,
+//     content:
+//       'The beginning of screenless design: UI jobs to be taken over by Solution Architect',
+//     createdAt: 1657432075944,
+//     isStarred: false,
+//     color: '#722ED1',
+//   },
+// ];
 
 function DocketNote() {
   const [notes, setNotes] = useState([]);
